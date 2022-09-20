3fecda44bbbcc413c8f2afca5e23e14c50fe1a6f
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-20 11:55:08 -0300

    TK-27 base infra ymls working

---
+++ infra/README.md
@@ -0,0 +1,2 @@
+# Resources
+This is for definitions of: database, roles, etc. This we don't usually create every time.
---
+++ services/trackings-api/functions/trackings.create.js
@@ -0,0 +1,4 @@
+// returns all trackings for a given user
+// - name: trackings.list
+// - Desc: All trackings of currentUser
+// - data: array of [id, name, lastOccurenceDate]
