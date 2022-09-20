51da67011dc253e52f0de62b04726c5c0a9cb7c8
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-20 17:44:19 -0300

    Commit before new attempt

--- services/v1/trackings-api/src/create/index.js
+++ services/v1/trackings-api/src/create/index.js
@@ -1,9 +1,15 @@
-module.exports.handler = async (event, context) => {
+import { nanoid } from 'nanoid';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+
+export const handler = async (event) => {
+	const { name } = JSON.parse(event.body);
+	console.log("DynamoDB is", DynamoDB)
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
 			message: 'create tracking!',
-			input: event,
+			input: { "name": name, id: nanoid() },
 		}),
 	}
 }
