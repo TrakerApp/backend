273e67e3e338ca9f3f5faca50174be4e61dd7c50
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-21 16:22:28 -0300

    Add webpack + redo dynamodb tables for queries performance; working test function

--- services/v1/trackings-api/src/create/index.js
+++ services/v1/trackings-api/src/create/index.js
@@ -1,15 +1,38 @@
 import { nanoid } from 'nanoid';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+
+const dynamodbClient = new DynamoDBClient({ region: 'us-east-1' });
 
 export const handler = async (event) => {
 	const { name } = JSON.parse(event.body);
-	console.log("DynamoDB is", DynamoDB)
+	const trackingId = nanoid();
 
-	return {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'create tracking!',
-			input: { "name": name, id: nanoid() },
-		}),
+	const params = {
+		TableName: process.env.TRACKINGS_TABLE,
+		Item: {
+			id: { S: trackingId },
+			name: { S: name },
+		}
+	}
+
+	try {
+		await dynamodbClient.send(new PutItemCommand(params))
+
+		return {
+			statusCode: 201,
+			body: JSON.stringify({
+				id: trackingId
+			}),
+		}
+	} catch (error) {
+		console.error("ERROR:", error)
+
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				message: 'error',
+				input: error,
+			}),
+		}
 	}
 }
