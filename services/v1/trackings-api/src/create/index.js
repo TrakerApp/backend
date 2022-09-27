496227d928d81a2489ac73f04a737c0c8d6dcb1f
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-27 20:28:46 -0300

    TK-27 separate infrastructure from functions so we have less risky deployments

--- services/v1/trackings-api/src/create/index.js
+++ services/v1/trackings-api/src/create/index.js
@@ -1,17 +1,25 @@
 import { nanoid } from 'nanoid';
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, PutItemCommand, GetItemCommand } from '@aws-sdk/client-dynamodb';
 
 const dynamodbClient = new DynamoDBClient({ region: 'us-east-1' });
 
 export const handler = async (event) => {
+	const userId = "asdf1234"; // temporal until we have cognito implemented in app
 	const { name } = JSON.parse(event.body);
+
 	const trackingId = nanoid();
 
 	const params = {
 		TableName: process.env.TRACKINGS_TABLE,
+		ConditionExpression: "attribute_not_exists(#c)",
+		ExpressionAttributeNames: {
+			"#c": "name"
+		},
 		Item: {
-			id: { S: trackingId },
+			userId: { S: userId },
 			name: { S: name },
+			trackingId: { S: trackingId },
+			createdAt: { N: `${(new Date()).getTime()}` }
 		}
 	}
 
@@ -21,8 +29,8 @@ export const handler = async (event) => {
 		return {
 			statusCode: 201,
 			body: JSON.stringify({
-				id: trackingId
-			}),
+				trackingId
+			})
 		}
 	} catch (error) {
 		console.error("ERROR:", error)
