4d18b531b2994268b9fa495be9b71f30c5914af7
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-10-04 20:15:41 -0300

    TK-27 migrate TIMESTAMP to TIMESTAMPTZ for fuckery fucking hell fix

--- migrations/20221001-1-create-tables.js
+++ migrations/20221001-1-create-tables.js
@@ -3,12 +3,15 @@ import { TABLE_NAME as TrackingTableName } from "../models/tracking.model.js"
 import { TABLE_NAME as OccurrenceTableName } from "../models/occurrence.model.js"
 
 const up = async () => {
+	// only if you need for local
+	// await Base.sql`DROP TABLE IF EXISTS ${Base.sql(OccurrenceTableName)}`
+	// await Base.sql`DROP TABLE IF EXISTS ${Base.sql(TrackingTableName)}`
 	await Base.sql`
 		CREATE TABLE IF NOT EXISTS ${Base.sql(TrackingTableName)} (
 			user_id STRING NOT NULL,
 			tracking_id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
 			name STRING NOT NULL,
-			last_occurrence_at TIMESTAMP,
+			last_occurrence_at TIMESTAMPTZ,
 			CONSTRAINT tracking_user_id_name_unique UNIQUE (user_id, name),
 			INDEX index_user_id (user_id)
 		)
@@ -18,7 +21,7 @@ const up = async () => {
 		CREATE TABLE IF NOT EXISTS ${Base.sql(OccurrenceTableName)} (
 			occurrence_id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
 			tracking_id UUID NOT NULL REFERENCES ${Base.sql(TrackingTableName)} (tracking_id),
-			created_at TIMESTAMP NOT NULL DEFAULT NOW()
+			created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 		)
 	`
 }
