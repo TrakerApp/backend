424c2d2c4f7dad64e6b4a88264bd3faf6869dd47
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-10-21 11:37:08 -0300

    TK-31 identify how to access custom attributes in lambda functions

--- libs/getUserId.js
+++ libs/getUserId.js
@@ -1,3 +1,31 @@
+// ALWAYS USE ID TOKEN IN FRONT FOR CALLS TO BACKEND
 export function getUserId(event) {
+	console.log("CLAIMS:", event.requestContext?.authorizer?.jwt?.claims)
 	return event.requestContext?.authorizer?.jwt?.claims?.sub
 }
+
+export function getUserEmail(event) {
+	return event.requestContext?.authorizer?.jwt?.claims?.email
+}
+
+export function getUserPlanName(event) {
+	return event.requestContext?.authorizer?.jwt?.claims?.['custom:plan_name']
+}
+
+/*
+ID TOKEN CLAIMS:
+  aud: 'adsfasdfasdf',
+  auth_time: '1666362361',
+  'cognito:username': 'thisistheuserID',
+  'custom:plan_name': 'beta',
+  email: 'emailhere',
+  email_verified: 'true',
+  event_id: 'some uuid',
+  exp: '1666365964',
+  iat: '1666362364',
+  iss: 'our pool ISS',
+  jti: 'jwt id',
+  origin_jti: 'jwt id 2?',
+  sub: 'thisistheuserID',
+  token_use: 'id'
+*/
